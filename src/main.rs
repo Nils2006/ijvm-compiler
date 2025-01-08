9504a3c56c68c83fa807f52b5764908d605e6516
Nils2006 <lag55weq@nils-schleutermann.de>
2025-01-08 13:17:51 +0100

    :sparkles: all functions implemented

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,49 @@ fn main() {
 
     let buffer = data.split("\n").collect::<Vec<&str>>();
 
+    let mut file_contents_base64: Vec<u8> = Vec::new();
+    file_contents_base64.push(0x00u8);
+
     for line in buffer {
         let instructions = line.split(" ").collect::<Vec<&str>>();
         for instruction in instructions {
             match instruction {
-                "BIPUSH" => println!("0x10"),
-                "IADD" => println!("0x60"),
-                _ => { println!("Unknown instruction: {}", instruction); },
+                "BIPUSH" => file_contents_base64.push(0x10),
+                "DUP" => file_contents_base64.push(0x59),
+                "GOTO" => file_contents_base64.push(0xA7),
+                "IADD" => file_contents_base64.push(0x60),
+                "IAND" => file_contents_base64.push(0x7E),
+                "IFEQ" => file_contents_base64.push(0x99),
+                "IFLT" => file_contents_base64.push(0x9B),
+                "IF_ICMPEQ" => file_contents_base64.push(0x9F),
+                "IINC" => file_contents_base64.push(0x84),
+                "ILOAD" => file_contents_base64.push(0x15),
+                "INVOKEVIRTUAL" => file_contents_base64.push(0xB6),
+                "IOR" => file_contents_base64.push(0x80),
+                "IRETURN" => file_contents_base64.push(0xAC),
+                "ISTORE" => file_contents_base64.push(0x36),
+                "ISUB" => file_contents_base64.push(0x64),
+                "LDC_W" => file_contents_base64.push(0x13),
+                "NOP" => file_contents_base64.push(0x00),
+                "POP" => file_contents_base64.push(0x57),
+                "SWAP" => file_contents_base64.push(0x5F),
+                "" => continue,
+                _ => {
+                    let signed_value = i8::from_str_radix(instruction, 16)
+                        .unwrap_or_else(|_| panic!("Invalid hex value: {}", instruction));
+
+                    println!("Signed value: {}", signed_value);
+
+                    // Konvertieren in u8, um es in den Vec zu pushen
+                    let unsigned_value = signed_value as u8;
+
+                    file_contents_base64.push(unsigned_value);
+
+                },
             }
         }
     }
+
+    let path = &args[1].replace("jbyte", "ijvm");
+    fs::write(path, file_contents_base64).unwrap();
 }
